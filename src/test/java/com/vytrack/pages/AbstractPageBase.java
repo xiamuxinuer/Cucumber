93197bc498296edc55487834497c227d2fd5cc51
xiamuxinuer <emmaxiaogaiti@gmail.com>
2020-04-12 11:02:26 -0500

    hi cucumber

---
+++ src/test/java/com/vytrack/pages/AbstractPageBase.java
@@ -0,0 +1,45 @@
+package com.vytrack.pages;
+
+
+import com.vytrack.utilities.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public abstract class AbstractPageBase {
+    protected WebDriver driver= Driver.getDriver();
+     protected WebDriverWait wait=new WebDriverWait(driver,15);
+
+    @FindBy(css = "#user-menu>a")
+   protected WebElement currentUser;
+
+
+
+    public AbstractPageBase(){
+        PageFactory.initElements(driver,this);
+    }
+
+    public void navigateTo(String tabName,String moduleName) throws InterruptedException {
+        String tabNameXpath = "//span[@class='title title-level-1' and contains(text(),'"+tabName+"')]";
+        String moduleXpath="//span[@class='title title-level-2' and contains(text(),'"+moduleName+"')]";
+
+        WebElement tabElement=driver.findElement(By.xpath(tabNameXpath));
+        WebElement moduleElement=driver.findElement(By.xpath(moduleXpath));
+
+        Actions actions=new Actions(driver);
+        Thread.sleep(5000);
+        actions.moveToElement(tabElement).pause(2000).click(moduleElement).build().perform();
+        Thread.sleep(5000);
+    }
+
+
+
+
+
+
+
+}
---
+++ src/test/java/com/vytrack/pages/LoginPage.java
@@ -0,0 +1,57 @@
+package com.vytrack.pages;
+
+
+import com.vytrack.utilities.BrowserUtilities;
+import com.vytrack.utilities.ConfigurationReader;
+import com.vytrack.utilities.Driver;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPage {
+    @FindBy(id = "prependedInput")
+    private WebElement username;
+
+    @FindBy(id = "prependedInput2")
+    private WebElement password;
+
+    @FindBy(id = "_submit")
+    WebElement loginButton;
+
+    @FindBy(linkText = "Forgot your password?")
+    public WebElement forgotPassword;
+
+    @FindBy(css = "[class='alert alert-error']")
+    private WebElement warningMessage;
+
+
+    public LoginPage() {
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+
+    public void login(String usernameValue, String passwordValue) {
+        username.sendKeys(usernameValue);
+        password.sendKeys(passwordValue, Keys.ENTER);
+        BrowserUtilities.waitForPageToLoad(10);
+        BrowserUtilities.wait(3);
+    }
+
+    public void login() {
+
+        username.sendKeys(ConfigurationReader.getProperty("store_manager"));
+        password.sendKeys(ConfigurationReader.getProperty("password"), Keys.ENTER);
+        BrowserUtilities.waitForPageToLoad(10);
+        BrowserUtilities.wait(3);
+    }
+
+
+
+    public String getWarningMessageText(){
+        return  warningMessage.getText();
+    }
+
+
+
+}
