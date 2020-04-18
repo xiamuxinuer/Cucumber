c1b659a7053a5a9ff2cd413fe1dd9a5a869f1ca0
xiamuxinuer <emmaxiaogaiti@gmail.com>
2020-04-18 14:57:23 -0500

    json report created in target file

--- src/test/java/com/vytrack/runners/CucumberRunner.java
+++ src/test/java/com/vytrack/runners/CucumberRunner.java
@@ -10,8 +10,9 @@ import org.junit.runner.RunWith;
         features = "src\\test\\resources\\features",
         dryRun = false,
         strict = true,
-        tags  ="@activities",
-        plugin = "html:target\\cucumber-html-report"
+        tags  ="",
+        plugin = {"html:target\\default-report",
+        "json:target\\cucumber.json"}
 )
 public class CucumberRunner {
 
