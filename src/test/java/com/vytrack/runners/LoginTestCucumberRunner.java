4f0ad5518a4a5b90000c12c3ce272cf666670a21
xiamuxinuer <emmaxiaogaiti@gmail.com>
2020-06-08 14:59:39 -0500

    driver class

--- src/test/java/com/vytrack/runners/LoginTestCucumberRunner.java
+++ src/test/java/com/vytrack/runners/LoginTestCucumberRunner.java
@@ -6,14 +6,14 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        glue = "com\\vytrack\\step_definitions",
-        features = "src\\test\\resources\\features",
+        glue = "com/vytrack/step_definitions",
+        features = "src/test/resources/features",
         dryRun = false,
         strict = true,
         tags  ="@driver",
-        plugin = {"html:target\\default-report",
-        "json:target\\cucumber1.json",
-        "rerun:target\\rerun_loginTest.txt"}
+        plugin = {"html:target/default-report",
+        "json:target/cucumber1.json",
+        "rerun:target/rerun_failedLoginTest.txt"}
 )
 public class LoginTestCucumberRunner {
 
